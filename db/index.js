1ada4572a32963814bd9cb0a8797f6307240f4d4
YKanakam <141025432+YKanakam@users.noreply.github.com>
2023-08-01 19:25:34 +0530

    Create index.js

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+// Configure the database connection
+const pool = new Pool({
+  user: 'username',
+  host: 'host',
+  database: 'db_name',
+  password: 'password',
+  port: 5432,
+});
+
+// Fetch items from the database
+async function getItems() {
+  const query = 'SELECT * FROM items';
+  const { rows } = await pool.query(query);
+  return rows;
+}
+
+module.exports = {
+  getItems,
+};
